249dc342518ecc33ac80f6675a68e26bdcda5a3c
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2022-03-17 17:04:51 +0530

    will add images

--- my-app/src/assets/index.js
+++ my-app/src/assets/index.js
@@ -1,8 +1,13 @@
 import Logo from "./Logo/Logo";
 import games from "./images/games.jpg"
 import controller from "./images/controller.jpg"
-import ps5 from "./images/ps5.jpg"
 import accessories from "./images/accessories.jpg"
+import ps5_pink from "./images/pink-ps5-controller.jpg"
+import xbox_series_x from "./images/xbox-series-x.jpg"
+import ps3 from "./images/ps3.jpeg"
+import ps5 from "./images/ps5.png"
+import ps4 from "./images/ps4.jpeg"
+import nintendo from "./images/nintendo-console.jpeg"
 
 
-export {Logo,games,controller,accessories,ps5}
+export {Logo,games,controller,accessories,ps5_pink,xbox_series_x,ps5,ps4,ps3,nintendo}
--- my-app/src/components/productpage/productlisting/ProductListing.jsx
+++ my-app/src/components/productpage/productlisting/ProductListing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../../../containers/home/Navbar";
 import { Category, FilterHeader, Range, Rating, SortBy } from "../filter";
-import { accessories, controller, games, ps5 } from "../../../assets";
+import { accessories, controller, games,ps5_pink,xbox_series_x ,ps5,ps4,ps3, nintendo} from "../../../assets";
 function ProductListing() {
   return (
     <div>
@@ -23,7 +23,34 @@ function ProductListing() {
             <div class="card card-product">
               <div class="card-img-body card-product-body">
                 <div class="card-product-img-cont">
-                  <img src={games} alt="" class="card-img-bd img-responsive" />
+                  <img src={ps3} alt="" class="card-img-bd img-responsive" />
+                </div>
+                <div class="card-body">
+                  <header class="card-header">
+                    <h3>Dragonwar Red Gear Gaming Mouse</h3>
+                    <div class="price-container">
+                      <h5>
+                        RATING:   4/5 <span class="font-light">(200)</span>
+                      </h5>
+                    </div>
+                    <div class="price-container">
+                      <h3>
+                        PRICE:   $25  {" "}
+                        <span class="text-strike">$30</span>
+                      </h3>
+                    </div>
+                  </header>
+                </div>
+              </div>
+              <div class="card-btn-container">
+                <button class="btn product-btn btn-s">Add To Wishlist</button>
+                <button class="btn product-btn btn-s">Add To Cart</button>
+              </div>
+            </div>
+            <div class="card card-product">
+              <div class="card-img-body card-product-body">
+                <div class="card-product-img-cont">
+                  <img src={xbox_series_x} alt="" class="card-img-bd img-responsive" />
                 </div>
                 <div class="card-body">
                   <header class="card-header">
@@ -77,7 +104,7 @@ function ProductListing() {
             <div class="card card-product">
               <div class="card-img-body card-product-body">
                 <div class="card-product-img-cont">
-                  <img src={accessories} alt="" class="card-img-bd img-responsive" />
+                  <img src={ps4} alt="" class="card-img-bd img-responsive" />
                 </div>
                 <div class="card-body">
                   <header class="card-header">
@@ -104,7 +131,34 @@ function ProductListing() {
             <div class="card card-product">
               <div class="card-img-body card-product-body">
                 <div class="card-product-img-cont">
-                  <img src={controller} alt="" class="card-img-bd img-responsive" />
+                  <img src={ps5_pink} alt="" class="card-img-bd img-responsive" />
+                </div>
+                <div class="card-body">
+                  <header class="card-header">
+                    <h3>Dragonwar Red Gear Gaming Mouse</h3>
+                    <div class="price-container">
+                      <h5>
+                        RATING:   4/5 <span class="font-light">(200)</span>
+                      </h5>
+                    </div>
+                    <div class="price-container">
+                      <h3>
+                        PRICE:   $25  {" "}
+                        <span class="text-strike">$30</span>
+                      </h3>
+                    </div>
+                  </header>
+                </div>
+              </div>
+              <div class="card-btn-container">
+                <button class="btn product-btn btn-s">Add To Wishlist</button>
+                <button class="btn product-btn btn-s">Add To Cart</button>
+              </div>
+            </div>
+            <div class="card card-product">
+              <div class="card-img-body card-product-body">
+                <div class="card-product-img-cont">
+                  <img src={nintendo} alt="" class="card-img-bd img-responsive" />
                 </div>
                 <div class="card-body">
                   <header class="card-header">
